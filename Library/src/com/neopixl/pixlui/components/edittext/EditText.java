eac94c4007bf74e7ce4dc7ec092d88843ae5f50d
Olivier Demolliens <olivier.demolliens@gmail.com>
2013-11-14 17:23:44 +0100

    [ANDROID] Add new methods on EditText

--- Library/src/com/neopixl/pixlui/components/edittext/EditText.java
+++ Library/src/com/neopixl/pixlui/components/edittext/EditText.java
@@ -1,9 +1,17 @@
 package com.neopixl.pixlui.components.edittext;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.view.ActionMode;
+import android.view.ActionMode.Callback;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.neopixl.pixlui.components.textview.FontFactory;
 import com.neopixl.pixlui.intern.PixlUIContants;
@@ -27,28 +35,94 @@ public class EditText extends android.widget.EditText {
 	}
 
 	private void setCustomFont(Context ctx, AttributeSet attrs) {
-		String typefaceName = attrs.getAttributeValue(PixlUIContants.SCHEMA_URL, EDITTEXT_ATTRIBUTE_FONT_NAME);
+		String typefaceName = attrs.getAttributeValue(
+				PixlUIContants.SCHEMA_URL, EDITTEXT_ATTRIBUTE_FONT_NAME);
 
-		if(typefaceName!=null){
-			setPaintFlags(this.getPaintFlags() | Paint.SUBPIXEL_TEXT_FLAG | Paint.LINEAR_TEXT_FLAG);
+		if (typefaceName != null) {
+			setPaintFlags(this.getPaintFlags() | Paint.SUBPIXEL_TEXT_FLAG
+					| Paint.LINEAR_TEXT_FLAG);
 			setCustomFont(ctx, typefaceName);
 		}
 	}
 
 	/**
 	 * Use this method to set a custom font in your code (/assets/fonts/)
+	 * 
 	 * @param ctx
-	 * @param Font Name, don't forget to add file extension
+	 * @param Font
+	 *            Name, don't forget to add file extension
 	 * @return
 	 */
 	public boolean setCustomFont(Context ctx, String font) {
 		Typeface tf = FontFactory.getInstance(ctx).getFont(font);
-		if(tf != null){
-			setTypeface(tf);  
+		if (tf != null) {
+			setTypeface(tf);
 			return true;
-		}else{
+		} else {
 			return false;
 		}
+	}
+
+	/**
+	 * Disable cut/copy/paste
+	 */
+	@SuppressLint("NewApi")
+	public void disableCopyAndPaste() {
+		if (android.os.Build.VERSION.SDK_INT < 11) {
+			this.setOnCreateContextMenuListener(new OnCreateContextMenuListener() {
+
+	            public void onCreateContextMenu(ContextMenu menu, View v,
+	                    ContextMenuInfo menuInfo) {
+	                menu.clear();
+
+	            }
+	        });
+		}else{
+			this.setCustomSelectionActionModeCallback(new Callback() {
+
+				public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+					return false;
+				}
+
+				public void onDestroyActionMode(ActionMode mode) {
+				}
+
+				public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+					return false;
+				}
+
+				public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+					return false;
+				}
+			});
+		}
+
+	}
+
+	@SuppressWarnings("deprecation")
+	@SuppressLint("NewApi")
+	/*
+	 * Cancel clipboard content
+	 */
+	public void cancelClipBoard() {
+
+		if (android.os.Build.VERSION.SDK_INT < 11) {
+			android.text.ClipboardManager clipboard = (android.text.ClipboardManager) getContext()
+					.getSystemService(Context.CLIPBOARD_SERVICE);
+			if (clipboard != null) {
+				clipboard.setText("");
+			}
+		} else {
+			android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getContext()
+					.getSystemService(Context.CLIPBOARD_SERVICE);
+			if (clipboard != null && clipboard.getPrimaryClip() != null
+					&& clipboard.getPrimaryClip().getItemCount() > 0) {
+				android.content.ClipData clip = android.content.ClipData
+						.newPlainText("", "");
+				clipboard.setPrimaryClip(clip);
+			}
+		}
+
 	}
 
 }
